beda3e5cc42bcfcf15bb3d9e734a3fdebf62a597
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-08-26 02:12:05 -0400

    Update for dynamic choices

--- internal/ui/simplemenu.go
+++ internal/ui/simplemenu.go
@@ -27,21 +27,51 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	
+
 	case tea.KeyMsg:
-		switch msg.String() {
+	switch msg.String() {
 
 		case "ctrl+c", "q":
 			m.quit = true
 			return m, tea.Quit
-		}
-	}
 
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		case "enter":
+			m.selected = m.choices[m.cursor]
+			return m, tea.Quit
+	}
+	}
 	return m, nil
 }
 
 func (m model) View() string {
-	s := "Program build success.\nCtrl-C or 'q' to quit.\n"
+	if m.quit && m.selected == "" {
+		return "Exiting program. Goodbye!\n"
+	}
+
+	if m.selected != "" {
+		return fmt.Sprintf("You selected: %s\n", m.selected)
+	}
+
+	s := "Select an option:\n\n"
+	for i, choice := range m.choices {
+		cursor := " " // null cursor
+		if m.cursor == i {
+			cursor = ">"
+		}
+		s += fmt.Sprintf("%s %s\n", cursor, choice)
+	}
+
+	s += "\nUse ↑/↓ or j/k       [enter]       q - quit\n"
 	return s
 }
 
